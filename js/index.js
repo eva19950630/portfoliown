2bb34208fdec4b27472eb255b9a3b42041b6701e
eva19950630 <fun910347@gmail.com>
2017-06-02 17:01:35 +0800

    add updateportinfo.html and add userinfo modal

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/*click update userinfo btn*/
+$(document).ready(function(){
+    $('#click-updateuserinfo').click(function() {
+        $('#updateuserinfo-modal').appendTo("body").modal('show');
+    });
+});
